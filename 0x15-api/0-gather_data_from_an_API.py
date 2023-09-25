bfcf59876e8c4944d72fa958718d4be68a0c18af
Mike-chege <mikechege93@gmail.com>
2023-09-25 02:27:51 -0700

    Fixed pep8 / Displaying the number of completed tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,9 +10,9 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    toDO = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [tl.get("title") for tl in toDO if tl.get("completed") is True]
+    completed = [tl.get("title") for tl in todos if tl.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(toDO)))
+        user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(complete)) for complete in completed]
