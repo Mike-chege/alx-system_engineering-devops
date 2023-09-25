31ac31d8b1ea5c5b75eba1d8c9919fc0d235f121
Mike-chege <mikechege93@gmail.com>
2023-09-25 02:08:30 -0700

    Reduced the number of words / fixed pep8 styling

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,16 +4,15 @@ This script returns information about a given employee (ID)
 Using REST API
 """
 
+import requests
+import sys
 
 if __name__ == "__main__":
-    import sys
-    import requests
-
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todo_list = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    t_list = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    status = [tl.get("title") for tl in todo_list if tl.get("status") is True]
+    status = [tl.get("title") for tl in t_list if tl.get("status") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(status), len(todo_list)))
+        user.get("name"), len(status), len(t_list)))
     [print("\t {}".format(s)) for s in status]
