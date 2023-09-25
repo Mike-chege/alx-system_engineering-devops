29bac80b8305123ed7c6c86267423a3fccf465d1
Mike-chege <mikechege93@gmail.com>
2023-09-25 02:57:54 -0700

    Fixed typos / pep8 / Enabled permissions

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,33 +3,16 @@
 This script returns information about a given employee (ID)
 Using REST API
 """
-import json
-import requests
-import sys
 
 if __name__ == "__main__":
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    try:
-        employee_id = sys.argv[1]
-    except KeyError:
-        print('Usage: {} employee_id'.format(sys.argv[0]))
-        exit(1)
+    import requests
+    import sys
 
-    url = base_url + 'users?id={}'.format(employee_id)
-    response = requests.get(url)
-    user = json.loads(response.text)
-    name = user[0].get('name')
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    toDO = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    url = base_url + 'todos?userId={}'.format(employee_id)
-    response = requests.get(url)
-    objs = json.loads(response.text)
-    completed = 0
-    completed_tasks = []
-    for obj in objs:
-        if obj.get('completed'):
-            completed_tasks.append(obj)
-            completed += 1
-
-    print("{} is done with tasks({}/{}):".format(name, completed, len(objs)))
-    for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
+    completed = [tl.get("title") for tl in toDO if tl.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(toDO)))
+    [print("\t {}".format(complete)) for complete in completed]
