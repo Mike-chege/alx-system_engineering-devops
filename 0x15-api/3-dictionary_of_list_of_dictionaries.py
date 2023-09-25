217107b3cf9d320dc61812ce57233f043be4a7ae
Mike-chege <mikechege93@gmail.com>
2023-09-25 08:33:52 -0700

    Exporting all employees records using json format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script export all employees
+Data in the JSON format
+"""
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    url = "https://jsonplaceholder.typicode.com/"
+    all_users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as json_FILE:
+            json.dump({
+                user.get("id"): [{
+                    "task": tl.get("title"),
+                    "completed": tl.get("completed"),
+                    "username": user.get("username")
+                } for tl in requests.get(url + "todos",
+                    params={"userId": user.get("id")}).json()]
+                for user in all_users}, json_FILE)
