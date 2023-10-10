349a0521f90d2e5a87a9971fe24a9ec515f59a95
Mike-chege <mikechege93@gmail.com>
2023-10-10 05:30:15 -0700

    Add subreddit validation & rate limit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,18 +9,18 @@ import requests
 
 def recurse(subreddit, hot_list=[], after="", count=0):
     """
-    Getting the list of hot posts
+    Get list of hot posts
     """
     base_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/mc_dev_)"
     }
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params,
+    response = requests.get(base_url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         return None
