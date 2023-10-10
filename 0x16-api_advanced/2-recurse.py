78d03c9af4e9be75070bfdb720d7673aa4df1026
Mike-chege <mikechege93@gmail.com>
2023-10-10 05:16:32 -0700

    Add subreddit validation & rate limit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -13,9 +13,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """
     base_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/mc_dev_)"
     }
     params = {
         "after": after,
