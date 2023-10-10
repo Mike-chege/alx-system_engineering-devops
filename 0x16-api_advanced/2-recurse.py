3b6f7ff828bad85fef554b6bf6fe48273c6b59fa
Mike-chege <mikechege93@gmail.com>
2023-10-10 04:00:07 -0700

    Recursive function

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,42 +1,59 @@
-#!/usr/bin//python3
-"""
-This function queries the Reddit API
-And returns a list containing the titles
-Of all hot articles for a given subreddit
-"""
-import json
+#!/usr/bin/python3
+
 import requests
-import sys
 
-
-def recurse(subreddit, hot_list=[]):
-    """api call to reddit to get the number of subscribers
-    """
-    base_url = 'https://www.reddit.com/r/{}/top.json'.format(
-        subreddit
-    )
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    if len(hot_list) == 0:
-        # grab info about all users
-        url = base_url
-    else:
-        url = base_url + '?after={}_{}'.format(
-            hot_list[-1].get('kind'),
-            hot_list[-1].get('data').get('id')
-        )
-    response = requests.get(url, headers=headers)
-    resp = json.loads(response.text)
-    try:
-        # grab the info about the users' tasks
-        data = resp.get('data')
-        children = data.get('children')
-    except:
+def recurse(subreddit, hot_list=[], after=None):
+    # Base case: If the subreddit is invalid, return None.
+    if subreddit is None:
         return None
-    if children is None or data is None or len(children) < 1:
+    
+    # Define the Reddit API URL for fetching hot articles.
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    
+    # Define the headers to mimic a User-Agent (Reddit requires this header).
+    headers = {"User-Agent": "MyRedditScraper"}
+    
+    # Define the parameters for the API request, including 'after' for pagination.
+    params = {"limit": 100, "after": after}
+    
+    # Make the API request.
+    response = requests.get(url, headers=headers, params=params)
+    
+    # Check if the request was successful.
+    if response.status_code != 200:
+        print(f"Error: Unable to fetch data for subreddit '{subreddit}'.")
+        return None
+    
+    # Parse the JSON response.
+    data = response.json()
+    
+    # Extract the 'children' containing the article data from the response.
+    children = data.get("data", {}).get("children", [])
+    
+    # Iterate through the articles and add their titles to the hot_list.
+    for child in children:
+        title = child.get("data", {}).get("title")
+        if title:
+            hot_list.append(title)
+    
+    # Check if there are more pages (more articles) to retrieve.
+    after = data.get("data", {}).get("after")
+    
+    # If there are more pages, recursively call the function.
+    if after:
+        return recurse(subreddit, hot_list, after)
+    else:
+        # If there are no more pages, return the hot_list.
         return hot_list
-    hot_list.extend(children)
-    return recurse(subreddit, hot_list)
+
+# Example usage:
+subreddit_name = "python"
+hot_articles = recurse(subreddit_name)
+
+if hot_articles is not None:
+    print(f"Hot articles in r/{subreddit_name}:")
+    for idx, title in enumerate(hot_articles, start=1):
+        print(f"{idx}. {title}")
+else:
+    print(f"No results found for subreddit '{subreddit_name}' or it is invalid.")
+
