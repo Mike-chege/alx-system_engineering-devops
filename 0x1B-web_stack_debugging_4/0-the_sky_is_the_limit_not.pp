b26d0f6b143feeefbd673de17046515de9a12336
Mike-chege <mikechege93@gmail.com>
2023-10-19 12:41:25 +0000

    Fixing huge amounts of requests failures

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# This script configures the web server to work under pressure
+# Preventing requests failure
+
+$cmd = 'sed -i "s/15/4096/" /etc/default/nginx'
+exec { 'fix_nginx':
+  command => $cmd,
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart nginx
+exec {'restart_nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
