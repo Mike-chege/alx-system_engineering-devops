ae66ed0f6f48be3d8f546e06df7d89cdf30283eb
Mike-chege <mikechege93@gmail.com>
2023-10-19 13:02:27 +0000

    Nginx configuration / Fixed typos

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
 # This script configures the web server to work under pressure
 
-$cmd = 'sed -i "s/15/4096/" /etc/default/nginx'
-exec { 'fix-nginx':
-  command => $cmd,
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
-# Restart nginx
-exec {'nginx-restart':
+# Restart Nginx
+exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
